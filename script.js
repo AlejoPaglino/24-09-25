8708d87bbe3eec7c2a31727b51e7f366b4c806fb
alumno <alumno@Instituto.local>
2025-10-02 11:17:34 -0300

    02/10/25

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let Partidos = document.querySelector(`#Partidos`)
 let dia = document.querySelector(`#dia`)
 let matchList = document.querySelector(`#matchList`)
 console.log('hola')
-fetch('https://api.football-data.org/v4/competitions/CL/matches?api_key=4f85f68caa7b4559bb4f73baf0a076ed&count=1')
-  .then(res => res.json())
+fetch('https://api.football-data.org/v4/competitions/CL/matches?api_key=0d41dcdd794a4e469cf82d9b363389e8&count=1')
+  .then(res => res.json(''))
   .then(datos => {
     fotos.src = datos[0].url
     titulo.textContent = datos[0].title
