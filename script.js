e7d88c09e6d8614247b7fedfeef485790b6c7a16
alumno <alumno@Instituto.local>
2025-10-08 08:24:42 -0300

    08/10/2025

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let fotos = document.querySelector('#fotos')
 let titulo = document.querySelector('#titulo')
-let Partidos = document.querySelector(`#Partidos`)
+let animales = document.querySelector(`#animales`)
 let dia = document.querySelector(`#dia`)
-let matchList = document.querySelector(`#matchList`)
+let listaDeAnimales = document.querySelector(`#listaDeAnimales`)
 console.log('hola')
-fetch('https://api.football-data.org/v4/competitions/CL/matches?api_key=0d41dcdd794a4e469cf82d9b363389e8&count=1')
+fetch('https://extinct-api.herokuapp.com/api/v1/animal/1')
   .then(res => res.json(''))
   .then(datos => {
     fotos.src = datos[0].url
