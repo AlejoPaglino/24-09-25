907d127b898113c54888989358af5f02d9280d21
AlejoPaglino <136344335+AlejoPaglino@users.noreply.github.com>
2025-10-14 21:29:56 -0300

    14/10/2025

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 let fotos = document.querySelector('#fotos')
 let titulo = document.querySelector('#titulo')
-let animales = document.querySelector(`#animales`)
-let dia = document.querySelector(`#dia`)
-let listaDeAnimales = document.querySelector(`#listaDeAnimales`)
+let animales = document.querySelector('#animales')
+let listaDeAnimales = document.querySelector('#listaDeAnimales')
+
 console.log('hola')
-fetch('https://extinct-api.herokuapp.com/api/v1/animal/')
-  .then(res => res.json(''))
-  .then(datos => {
-    fotos.src = datos[0].url
-    titulo.textContent = datos[0].title
-    Partidos.textContent = datos[0].date
-    dia.textContent = datos[0].date
-    matchList.textContent = datos[0].date
-    console.log(datos)
-  })
 
-
-  fetch("https://cheba-apis.vercel.app/extinct")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log("Respuesta de la API:", data);
-  })
-  .catch(error => {
-    console.error("Error al hacer la petición:", error);
-  });
-
- 
+animales.addEventListener('click', function() {
+    fetch('https://extinct-api.herokuapp.com/api/v1/animal/')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(info => {
+       
+        let animalAleatorio = info.data[0];
+        console.log('Animal aleatorio:', animalAleatorio);
+        fotos.src = animalAleatorio.imageSrc;//muestra la imagen en el html
+        fotos.alt = animalAleatorio.commonName;// esto muestra el nombre
+        titulo.textContent = animalAleatorio.commonName;
+        
+       let nuevoItem = document.createElement('ul');// crea nueva linea en la lista
+        nuevoItem.textContent = `${animalAleatorio.commonName} - ${animalAleatorio.location} (${animalAleatorio.lastRecord})`;//muestra valores en pantalla
+        listaDeAnimales = (nuevoItem);//asignar un valor
+        
+    })
+    .catch(error => {
+        console.error("Error al hacer la petición:", error);
+        alert('Error al cargar el animal. Intenta nuevamente.');
+    });
+});
